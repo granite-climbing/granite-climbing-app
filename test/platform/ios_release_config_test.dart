0b3a59f066a33b1d5e6a40cdc335ce5323c5c5d3
scorchedrice <wldnd2977@gmail.com>
2026-08-17 13:41:16 +0900

    chore: prepare iOS 1.1.2 build 26

--- test/platform/ios_release_config_test.dart
+++ test/platform/ios_release_config_test.dart
@@ -3,15 +3,17 @@ import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('TestFlight release uses version 1.1.2 build 25 and iOS 15', () {
+  test('TestFlight release uses version 1.1.2 build 26 and iOS 15', () {
     final pubspec = File('pubspec.yaml').readAsStringSync();
     final podfile = File('ios/Podfile').readAsStringSync();
     final project =
         File('ios/Runner.xcodeproj/project.pbxproj').readAsStringSync();
+    final infoPlist = File('ios/Runner/Info.plist').readAsStringSync();
 
-    expect(pubspec, contains('version: 1.1.2+25'));
+    expect(pubspec, contains('version: 1.1.2+26'));
     expect(podfile, contains("platform :ios, '15.0'"));
     expect(podfile, isNot(contains("platform :ios, '13.0'")));
+    expect(infoPlist, contains('<string>graniteclimbing</string>'));
 
     final deploymentTargets = RegExp(
       r'IPHONEOS_DEPLOYMENT_TARGET = ([^;]+);',
